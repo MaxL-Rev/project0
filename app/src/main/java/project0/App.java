8ecd1e0850d03c3a6775874ef158a8b45e8206de
Maxwell Lee <maxwell.lee@revature.net>
2021-03-26 19:08:17 -0700

    Created shopping list class and added a rudimentary menu to interact with it

--- app/src/main/java/project0/App.java
+++ app/src/main/java/project0/App.java
@@ -3,12 +3,93 @@
  */
 package project0;
 
-public class App {
-    public String getGreeting() {
+import java.util.Scanner;
+
+public class App 
+{
+    public String getGreeting() 
+    {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println(new App().getGreeting());
+
+        ShoppingList shoppingList = new ShoppingList();
+        Scanner input = new Scanner(System.in);
+
+        boolean continueWriting = true;
+        int menuSelection = 0;
+        while(continueWriting)
+        {
+            displayInitialMessage(shoppingList);
+            menuSelection = menu(input);
+
+            switch(menuSelection)
+            {
+                case 1:
+                    editTitle(shoppingList, input);
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+                case 5:
+                    continueWriting = false;
+                    break;
+            }
+        }
+    }
+
+    private static void displayInitialMessage(ShoppingList shoppingList)
+    {
+        if(shoppingList.isEmpty())
+        {
+            System.out.println("Looks like you don't have a list yet!");
+        }
+        else
+        {
+            shoppingList.displayList();
+        }
+        System.out.println();
+    }
+
+    private static int menu(Scanner input)
+    {
+        int menuSelection = 0;
+
+        do
+        {
+            System.out.println("Please choose your action:");
+            System.out.println("1: Edit title of an item");
+            System.out.println("2: Edit body of an item");
+            System.out.println("3: Delete an item");
+            System.out.println("4: Check an item");
+            System.out.println("5: Quit");
+            System.out.print("What did you want to do?(1-5): ");
+
+            menuSelection = input.nextInt();
+        }while(menuSelection < 1 || menuSelection > 5);
+
+        System.out.println();
+        return menuSelection;
+    }
+
+    private static void editTitle(ShoppingList shoppingList, Scanner input)
+    {
+        int index = 0;
+        do
+        {
+            System.out.print("What number item did you want to edit?(1-" + shoppingList.size() + "): ");
+
+            index = input.nextInt();
+        }while(index < 1 || index > shoppingList.size());
+        
     }
 }
---
+++ app/src/main/java/project0/ShoppingList.java
@@ -0,0 +1,139 @@
+package project0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingList
+{
+    private List<ListItem> shoppingList;
+    private int checkedIndex;
+
+    public ShoppingList()
+    {
+        shoppingList = new ArrayList<>();
+        checkedIndex = 0;
+    }
+
+    public void addItem(String title, String body)
+    {
+        shoppingList.add(new ListItem(title, body));
+        checkedIndex++;
+    }
+
+    public void editItemTitle(int index, String newTitle)
+    {
+        shoppingList.get(index).setTitle(newTitle);
+    }
+
+    public void editItemBody(int index, String newBody)
+    {
+        shoppingList.get(index).setBody(newBody);
+    }
+
+    public void deleteItem(int index)
+    {
+        shoppingList.remove(index);
+        checkedIndex--;
+    }
+
+    public boolean isEmpty()
+    {
+        return shoppingList.isEmpty();
+    }
+
+    public int size()
+    {
+        return shoppingList.size();
+    }
+
+    public int getCheckedIndex()
+    {
+        return checkedIndex;
+    }
+
+    public void checkItem(int index)
+    {
+        shoppingList.add(shoppingList.get(index));
+        deleteItem(index);
+
+        checkedIndex--;
+    }
+
+    public void displayList()
+    {
+        for(int i = 0; i < size(); i++)
+        {
+            StringBuilder line = new StringBuilder("");
+            
+            //System.out.print((i+1) + "- ");
+            line.append((i+1) + "- ");
+            if(i < checkedIndex)
+            {
+                line.append("[ ] ");
+            }
+            else
+            {
+                line.append("[x] ");
+            }
+            line.append(shoppingList.get(i).getTitle() + ": " + shoppingList.get(i).getBody());
+            //System.out.println(shoppingList.get(i).getTitle() + ": " + shoppingList.get(i).getBody());
+            System.out.println(line);
+        }
+    }
+
+    /*
+    public void displayActive()
+    {
+        for(int i = 0; i < checkedIndex; i++)
+        {
+            System.out.println(i + ": " + shoppingList.get(i));
+        }
+    }
+
+    public void displayChecked()
+    {
+        for(int i = checkedIndex; i < shoppingList.size(); i++)
+        {
+            System.out.println((i - checkedIndex + 1) + ": " + shoppingList.get(i));
+        }
+    }
+    */
+    
+    private class ListItem
+    {
+        private String title;
+        private String body;
+
+        public ListItem(String title, String body)
+        {
+            this.title = title;
+            this.body = body;
+        }
+
+        public String getTitle()
+        {
+            return title;
+        }
+
+        public String getBody()
+        {
+            return body;
+        }
+
+        public void setTitle(String title)
+        {
+            this.title = title;
+        }
+
+        public void setBody(String body)
+        {
+            this.body = body;
+        }
+
+        @Override
+        public String toString()
+        {
+            return getTitle() + ": " + getBody();
+        }
+    }
+}

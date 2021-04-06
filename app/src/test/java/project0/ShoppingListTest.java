5ea9e237c4e53964f8745de97b40c0ca48cc1282
Maxwell Lee <maxwell.lee@revature.net>
2021-04-06 10:57:31 -0700

    Made some tests for my data structures as practice

--- app/src/test/java/project0/ShoppingListTest.java
+++ app/src/test/java/project0/ShoppingListTest.java
@@ -1,5 +1,51 @@
 package project0;
 
-public class ShoppingListTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ShoppingListTest 
+{
+    private static final String[] titles = {"Banana", "Apples", "Cough Meds", "Steak"};
+    private static final String[] bodies = {"Grab 2", "4", "Elderberry spray", "Ribeyes"};
+
+    @Test public void testShoppingListAddItem() 
+    {
+        ShoppingList shoppingList = new ShoppingList("Testing Add List");
+        assertEquals("New shopping list should have size of 0", 0, shoppingList.size());
+
+        shoppingList.addItem("Test Title", "Test Body");
+        assertEquals("Shopping list should have size of 1", 1, shoppingList.size());
+
+        populateShoppingList(shoppingList);
+
+        assertEquals("Shopping list should have size of "+(1+titles.length), (1+titles.length), shoppingList.size());
+    }
     
+    @Test public void testShoppingListDeleteItem()
+    {
+        ShoppingList shoppingList = new ShoppingList("Testing Delete List");
+        populateShoppingList(shoppingList);
+        
+        shoppingList.deleteItem(0);
+
+        assertEquals("Shopping list should have size of "+(titles.length-1)+" after 1 deletion", (titles.length-1), shoppingList.size());
+    }
+
+    @Test public void testShoppingListCheckedItem()
+    {
+        ShoppingList shoppingList = new ShoppingList("Testing Check List");
+        populateShoppingList(shoppingList);
+
+        shoppingList.checkItem(0);
+
+        assertEquals("Shopping list's checked index should be "+ (titles.length-1), (titles.length-1), shoppingList.getCheckedIndex());
+    }
+
+    private void populateShoppingList(ShoppingList shoppingList)
+    {
+        for(int i = 0; i < titles.length; i++)
+        {
+            shoppingList.addItem(titles[i], bodies[i]);
+        }
+    }
 }
